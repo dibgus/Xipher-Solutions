e02f4a1c27207693b05cbac0b193d5097de157b6
dibgus <dibgus@users.noreply.github.com>
2015-10-18 07:24:27 -0600

    Added initial project layout
    ./Project/Encrypta: Front-end C# code (written in Visual Studio as a
    form application)
    ./Project/Backend: Back-end C++ code (written in Eclipse Mars and CDT)

---
+++ Project/backend/src/EncryptaBackend.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : EncryptaBackend.cpp
+// Author      : Ivan Krukov
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
