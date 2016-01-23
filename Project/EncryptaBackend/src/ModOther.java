170beab64b7c39d95a9375c83d6bb5390e7e508e
dibgus <dibgus@users.noreply.github.com>
2016-01-22 21:04:13 -0700

    ModOther changes and .gitignore additions

--- Project/EncryptaBackend/src/ModOther.java
+++ Project/EncryptaBackend/src/ModOther.java
@@ -1,11 +1,41 @@
 /**
  * Created by ikrukov on 1/14/2016.
  */
-public class ModOther {
-    public static String performOperation(String expression, String flag, boolean isEncryping)
-    {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.*;
 
-        return "";
+public class ModOther {
+    public static String performOperation(String expression, String flag, boolean encrypting)
+    {
+        String evaluated = "";
+        String[] command = InputHandler.handleKey(flag, "="); //splits passed command into the main command and any cipher/etc that should be used
+        switch(command[0])
+        {
+            case "compress":
+
+                break;
+            default:
+                System.err.println("Could not find other operation: " + command[0]);
+                break;
+        }
+        return evaluated;
+    }
+
+    public static String compressExpression(String expression)
+    {
+        String compressed = "";
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        byte[] stringData = expression.getBytes();
+        try {
+            GZIPOutputStream compressor = new GZIPOutputStream(byteStream, stringData.length);
+            compressor.write(stringData, 0, stringData.length);
+            compressor.close();
+            byteStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return byteStream.toString();
     }
 
 }
